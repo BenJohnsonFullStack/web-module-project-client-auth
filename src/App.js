f0b08fb14798b7f4843851f17c83145456ae4012
Ben Johnson <monk.sound.design@gmail.com>
2023-07-13 17:25:04 -0400

    FriendsList and AddFriend setup

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import FriendsList from "./components/FriendsList";
+import AddFriends from "./components/AddFriends";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Login />} />
-        {/* <Route path="/friends" element={<FriendsList />} /> */}
-        {/* <Route path="/friends/add" element={<AddFriends/>} /> */}
+        <Route path="/friends" element={<FriendsList />} />
+        <Route path="/friends/add" element={<AddFriends />} />
       </Routes>
     </div>
   );
