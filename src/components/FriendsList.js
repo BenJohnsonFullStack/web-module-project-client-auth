706c7ef97dff1cd60e18fcdc45add503f1f66181
Ben Johnson <monk.sound.design@gmail.com>
2023-07-13 17:32:19 -0400

    friendslist setup

--- src/components/FriendsList.js
+++ src/components/FriendsList.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const FriendsList = () => {
-  return <></>;
+  return (
+    <>
+      <h1>FRIENDS LIST</h1>
+      <div className="friends">{/* map over friends and return */}</div>
+    </>
+  );
 };
 
 export default FriendsList;
