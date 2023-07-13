f1320aebb90bbe7c7c22dc4a85fd18ef38ec755e
Ben Johnson <monk.sound.design@gmail.com>
2023-07-13 16:16:12 -0400

    styles app.css

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,28 +2,34 @@ import React from "react";
 
 function Login() {
   return (
-    <div className="navbar">
-      <h2>FRIENDS DATABASE</h2>
-      <div className="nav-links">
-        <button className="nav-button">LOGIN.</button>
-        <button className="nav-button">FRIENDLIST.</button>
-        <button className="nav-button">ADDFRIEND.</button>
-        <button className="nav-button">LOGOUT</button>
+    <>
+      <div className="navbar">
+        <h2>FRIENDS DATABASE</h2>
+        <div className="nav-links">
+          <button className="nav-button">LOGIN.</button>
+          <button className="nav-button">FRIENDLIST.</button>
+          <button className="nav-button">ADDFRIEND.</button>
+          <button className="nav-button">LOGOUT</button>
+        </div>
       </div>
       <div className="spacer"></div>
-      <h1>LOGIN</h1>
-      <form>
-        <label>
-          USERNAME
-          <input type="text" name="username"></input>
-        </label>
-        <label>
-          PASSWORD
-          <input type="text" name="password"></input>
-        </label>
-        <button type="submit">SUBMIT</button>
-      </form>
-    </div>
+      <div className="form-container">
+        <h1>LOGIN</h1>
+        <form>
+          <div className="input-fields">
+            <div className="input-field">
+              <label>USERNAME</label>
+              <input type="text" name="username"></input>
+            </div>
+            <div className="input-field">
+              <label>PASSWORD</label>
+              <input type="text" name="password"></input>
+            </div>
+          </div>
+          <button type="submit">SUBMIT</button>
+        </form>
+      </div>
+    </>
   );
 }
 
